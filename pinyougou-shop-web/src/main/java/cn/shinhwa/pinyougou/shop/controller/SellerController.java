049851249e6e370bff0c5d85a8e0be67f104a5c5
Shinhwa.W <443532166@qq.com>
2018-11-10 00:43:56 +0800

    complete password encrypt by BcryptEncoder

--- pinyougou-shop-web/src/main/java/cn/shinhwa/pinyougou/shop/controller/SellerController.java
+++ pinyougou-shop-web/src/main/java/cn/shinhwa/pinyougou/shop/controller/SellerController.java
@@ -5,6 +5,7 @@ import cn.shinhwa.pinyougou.sellergoods.service.SellerService;
 import com.alibaba.dubbo.config.annotation.Reference;
 import entity.PageResult;
 import entity.Result;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,6 +53,11 @@ public class SellerController {
      */
     @RequestMapping("/add")
     public Result add(@RequestBody TbSeller seller) {
+
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String password = passwordEncoder.encode(seller.getPassword());
+        seller.setPassword(password);
+
         try {
             sellerService.add(seller);
             return new Result(true, "增加成功");
