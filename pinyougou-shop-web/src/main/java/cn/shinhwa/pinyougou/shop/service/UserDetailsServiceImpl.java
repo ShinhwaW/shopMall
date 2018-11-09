7a7843c5cbffc31aeddcf8353b521ee1206300c2
ShinhwaW <443532166@qq.com>
2018-11-09 23:41:45 +0800

    update security login control

---
+++ pinyougou-shop-web/src/main/java/cn/shinhwa/pinyougou/shop/service/UserDetailsServiceImpl.java
@@ -0,0 +1,39 @@
+package cn.shinhwa.pinyougou.shop.service;
+
+import cn.shinhwa.pinyougou.pojo.TbSeller;
+import cn.shinhwa.pinyougou.sellergoods.service.SellerService;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private SellerService sellerService;
+
+    public void setSellerService(SellerService sellerService) {
+        this.sellerService = sellerService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_SELLER"));
+
+        TbSeller seller = sellerService.findOne(username);
+        if (seller != null) {
+            if (seller.getStatus().equals("1")) {
+                return new User(username, seller.getPassword(), grantedAuthorities);
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
+}
